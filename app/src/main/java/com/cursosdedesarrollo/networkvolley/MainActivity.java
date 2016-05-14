643b9d18a3cbef7be6aace0c565867398c7b49d6
pepesan <pepesan@gmail.com>
2016-05-14 21:33:53 +0200

    puesto un singleton para manejar la cola de peticiones e incluida la funcionalidad de coger una imagen

--- app/src/main/java/com/cursosdedesarrollo/networkvolley/MainActivity.java
+++ app/src/main/java/com/cursosdedesarrollo/networkvolley/MainActivity.java
@@ -1,16 +1,24 @@
 package com.cursosdedesarrollo.networkvolley;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -20,11 +28,14 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class
             .getSimpleName();
     private RequestQueue mRequestQueue;
+    private ImageView mImageView;
+    private String URL2 = "http://i.imgur.com/7spzG.png";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.textView);
+        mImageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void lanza(View v){
@@ -44,11 +55,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         getRequestQueue();
-        mRequestQueue.add(stringRequest);
+        addToRequestQueue(stringRequest,TAG);
+    }
+    public void cogeImagen(View v){
+        Log.d("app","petición de imagen lanzada");
+        ImageRequest request = new ImageRequest(URL2,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        mImageView.setImageResource(R.mipmap.ic_launcher);
+                    }
+                });
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(request);
     }
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+            // Instantiate the cache
+            Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+// Set up the network to use HttpURLConnection as the HTTP client.
+            Network network = new BasicNetwork(new HurlStack());
+
+// Instantiate the RequestQueue with the cache and network.
+            mRequestQueue = new RequestQueue(cache, network);
+            // Start the queue
+            mRequestQueue.start();
         }
 
         return mRequestQueue;
@@ -67,6 +104,13 @@ public class MainActivity extends AppCompatActivity {
     public void cancelPendingRequests(Object tag) {
         if (mRequestQueue != null) {
             mRequestQueue.cancelAll(tag);
+        }
+    }
+    @Override
+    protected void onStop () {
+        super.onStop();
+        if (mRequestQueue != null) {
+            cancelPendingRequests(TAG);
         }
     }
 }
